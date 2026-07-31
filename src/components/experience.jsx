badbd8a72dd1d26eedf6bc8bd452242f7192e47c
jenaro fv <jen4r0@gmail.com>
2026-07-31 08:01:32 -0600

    add empty experience component

---
+++ src/components/experience.jsx
@@ -0,0 +1,9 @@
+import "../styles/styles.css";
+
+export function ProfessionalExperience ({jobTitle, company, startDate, endDate, description}) {
+  return (
+    <>
+      <h2>Professional Experience</h2>
+    </>
+  )
+}
